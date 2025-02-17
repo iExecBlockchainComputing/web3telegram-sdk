60e30db921879f3638d1e132310c63b08ff5bda6
Abbes Benayache <benayache.abbes@gmail.com>
2025-02-17 21:31:24 +0100

    fix e2e error : use web3telegram-test app address

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 // TODO : use prod address when available (not yet deployed)
 export const WEB3TELEGRAM_DAPP_ADDRESS =
-  '0x796c4CcE0535C59527cdE4a7cb185465248DbAAb'; // web3telegram-test.apps.iexec.eth
+  '0xb3abe56470f23a5a5b6ab28fc6554b2a7dc1ba41'; // web3telegram-test.apps.iexec.eth
 
 // TODO : use prod address when available (not yet deployed)
 export const WHITELIST_SMART_CONTRACT_ADDRESS =
