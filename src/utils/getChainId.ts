ab33c4259be46c3af4c4adc79d59e2cd3cd6d131
paypes <benayache.abbes@gmail.com>
2025-07-02 16:05:18 +0200

    feat(utils): add getChainIdFromProvider util

---
+++ src/utils/getChainId.ts
@@ -0,0 +1,46 @@
+import {
+  JsonRpcProvider,
+  BrowserProvider,
+  AbstractProvider,
+  AbstractSigner,
+  Eip1193Provider,
+} from 'ethers';
+import { DEFAULT_CHAIN_ID } from '../config/config.js';
+
+type EthersCompatibleProvider =
+  | string
+  | AbstractProvider
+  | AbstractSigner
+  | Eip1193Provider;
+
+export async function getChainIdFromProvider(
+  ethProvider: EthersCompatibleProvider
+): Promise<number> {
+  try {
+    if (typeof ethProvider === 'string') {
+      // Handle network string
+      if (ethProvider === 'bellecour') return 134;
+      // TODO add arbitrum & avalanche
+      const provider = new JsonRpcProvider(ethProvider);
+      const network = await provider.getNetwork();
+      return Number(network.chainId);
+    } else if (ethProvider instanceof AbstractProvider) {
+      const network = await ethProvider.getNetwork();
+      return Number(network.chainId);
+    } else if (ethProvider instanceof AbstractSigner) {
+      const { provider } = ethProvider;
+      if (!provider) {
+        throw Error('Signer is not connected to a provider');
+      }
+      const network = await provider.getNetwork();
+      return Number(network.chainId);
+    } else if ('request' in ethProvider) {
+      const provider = new BrowserProvider(ethProvider as Eip1193Provider);
+      const network = await provider.getNetwork();
+      return Number(network.chainId);
+    }
+  } catch (e) {
+    console.warn('Failed to detect chainId:', e);
+  }
+  return DEFAULT_CHAIN_ID;
+}
