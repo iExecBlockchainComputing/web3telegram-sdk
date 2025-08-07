dab0f77fa799ffd2a91a23d1cbe21687623d48bb
paypes <43441600+abbesBenayache@users.noreply.github.com>
2025-08-07 10:55:23 +0200

    style: format codebase using npm run format

--- deployment-dapp/src/singleFunction/deployApp.ts
+++ deployment-dapp/src/singleFunction/deployApp.ts
@@ -30,7 +30,7 @@ export const deployApp = async ({
 
   const mrenclave = {
     framework: 'SCONE' as any, // workaround framework not auto capitalized
-    version: `v${sconifyVersion.split(".").slice(0, 2).join(".")}`, // extracts "vX.Y" from "X.Y.Z-vN" format (e.g., "5.9.1-v16" → "v5.9")
+    version: `v${sconifyVersion.split('.').slice(0, 2).join('.')}`, // extracts "vX.Y" from "X.Y.Z-vN" format (e.g., "5.9.1-v16" → "v5.9")
     entrypoint: 'node /app/app.js',
     heapSize: 1073741824,
     fingerprint,
