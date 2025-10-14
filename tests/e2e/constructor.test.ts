997c71db07fe71edeea1404ba8072a6d80be0219
Le-Caignec <robinlecaignec@yahoo.fr>
2025-10-14 12:12:03 +0200

    fix: update dependencies and remove experimental flag from config

--- tests/e2e/constructor.test.ts
+++ tests/e2e/constructor.test.ts
@@ -131,7 +131,7 @@ describe('IExecWeb3telegram()', () => {
     MAX_EXPECTED_WEB2_SERVICES_TIME
   );
 
-  describe('When instantiating SDK with an experimental network', () => {
+  describe.skip('When instantiating SDK with an experimental network', () => {
     const experimentalNetworkSigner = getWeb3Provider(
       Wallet.createRandom().privateKey,
       {
